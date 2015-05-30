cbb9ba6b47d7bb54a25666c9470f825e0ad1ab30
Enrique Fernandez <efcasado@gmail.com>
2015-05-30 16:00:04 +0200

    Implement edocstring parse transform

--- src/edocstrings_ptransform.erl
+++ src/edocstrings_ptransform.erl
@@ -8,10 +8,68 @@
 %%%=========================================================================
 -module(edocstrings_ptransform).
 
+-compile({parse_transform, forms_pt}).
+
 -export([parse_transform/2]).
 
 
 %% Exported functions
 %%=========================================================================
 parse_transform(Forms, _Opts) ->
-    Forms.
+    Docstrings = fetch_docstrings(Forms),
+    HelpF      = gen_help_function(Docstrings),
+    meta:add_function(HelpF, true, Forms).
+
+
+%% Local functions
+%%=========================================================================
+fetch_docstrings(Forms) ->
+    forms:reduce(
+      fun(Form, Acc) ->
+              case has_docstring(Form) of
+                  {true, FDocstring}  ->
+                      [FDocstring| Acc];
+                  false ->
+                      Acc
+              end
+      end,
+      [],
+      Forms).
+
+has_docstring({function, _Line, Name, Arity, [C |_Cs]}) ->
+    %% if a function has more than one clause, the docstring must be
+    %% defined in the first clause.
+    case fclause_has_docstring(C) of
+        {true, Docstring} ->
+            {true, {{Name, Arity}, Docstring}};
+        false             ->
+            false
+    end;
+has_docstring(_Form) ->
+    false.
+
+fclause_has_docstring({clause, _Line, _Args, _Guards, Body}) ->
+    fbody_has_docstring(Body).
+
+fbody_has_docstring(Body = [{string, _Line, Docstring}| _])
+  when length(Body) > 1 ->
+    {true, Docstring};
+fbody_has_docstring(_Body) ->
+    %% if the function body has only 1 expression, that function cannot
+    %% have a docstring.
+    false.
+
+gen_help_function(Docstrings) ->
+    Bindings = [
+                 [
+                  {'F', F},
+                  {'A', A},
+                  {'D', D}
+                 ]
+                || {{F, A}, D} <- Docstrings ],
+    forms:function(
+      help,
+      [
+        {fun(F, A) -> D end, Bindings},
+        {fun(_, _) -> {error, docstring_not_found} end, []}
+      ]).
